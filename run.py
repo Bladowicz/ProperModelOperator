c121b98d3f43e9efce7c7fac8dba35ce06e70b55
Grzegorz Baranowski <grzegorz.baranowski@adpilot.pl>
2015-12-31 10:48:08 +0100

    Not working

--- run.py
+++ run.py
@@ -2,9 +2,21 @@
 import Operators as O
 import os
 import sys
+import multiprocessing as mp
+import glob
+
+class rabbitProccess(mp.Process):
+
+    def __init__(self, config):
+        self.config = config
+        mp.Process.__init__(self)
+
+    def run(self):
+        start_rabbit(self.config)
 
 
-def main(config_file):
+
+def start_rabbit(config_file):
     controler = O.ModelOperator(config_file)
     controler.register()
     controler.verify()
@@ -12,10 +24,18 @@ def main(config_file):
     controler.run()
 
 
+# def main(location):
+#     rabbits = []
+#     for filename in glob.glob(os.path.join(location, "*.ini")):
+#         rabbits.append(rabbitProccess(filename))
+#     [rabbit.start() for rabbit in rabbits]
+#     [rabbit.join() for rabbit in rabbits]
 
 
 
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    location = os.path.abspath(sys.argv[1])
+    # main(location)
+    start_rabbit(os.path.join(location, sys.argv[1]))
